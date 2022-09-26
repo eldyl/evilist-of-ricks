97085303d523deeb5de2772b9f49506fca2f719b
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-09-25 23:49:21 -0700

    rick vs rick render

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,11 +2,15 @@ import RickCard from '../RickCard';
 
 export default function Layout() {
   return (
-    <div className='flex flex-col p-6'>
-      <h2 className='font-bold text-5xl text-center pb-6'>
+    <div className='flex flex-col p-6 w-screen'>
+      <h2 className='font-bold text-2xl sm:text-5xl text-center pb-6'>
         Who is the most evil Rick?
       </h2>
-      <RickCard />
+      <div className='sm:flex mx-auto gap-6 text-center'>
+        <RickCard />
+        <h2 className='sm:flex sm:items-center'>OR</h2>
+        <RickCard />
+      </div>
     </div>
   );
 }
--- components/RickCard/index.tsx
+++ components/RickCard/index.tsx
@@ -19,33 +19,21 @@ export default function RickCard() {
     getData();
   }, []);
 
-  let results = ricks.map((rick) => {
-    return (
-      <div
-        key={rick.id}
-        className='flex flex-col bg-neutral-300 gap-3 text-center text-gray-600 rounded-lg shadow-lg p-6'
-      >
+  return (
+    <div className='mx-auto'>
+      <div className='flex flex-col bg-neutral-300 gap-3 text-center text-gray-600 rounded-lg shadow-lg p-6'>
         <Image
-          src={rick.image}
-          alt={rick.name}
+          src={'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}
+          alt={'Rick'}
           width={250}
           height={250}
           className='rounded-lg'
         />
-        <h3 className=' text-xl text font-medium'>{rick.name}</h3>
+        <h3 className=' text-xl text font-medium'>{'Rick Sanchez'}</h3>
         <button className='p-3 bg-neutral-400 rounded-md font-bold text-black'>
           PURE EVIL
         </button>
       </div>
-    );
-  });
-
-  return (
-    <div className='flex flex-col gap-6 mx-auto'>{results}</div>
-    // <div>
-    //   <image />
-    //   <h3></h3>
-    //   <button></button>
-    // </div>
+    </div>
   );
 }

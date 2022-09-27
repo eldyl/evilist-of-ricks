1c556ae8ad2984cebb4fc1e5fc1b142f3acaaee5
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-09-27 08:41:24 -0700

    trpc is happy, better conditional render

--- src/components/RickCard/index.tsx
+++ src/components/RickCard/index.tsx
@@ -10,28 +10,32 @@ export default function RickCard(props: Rick) {
   let { name, image } = props;
 
   return (
-    <div className='mx-auto'>
-      <div className='flex flex-col bg-neutral-300 gap-3 text-center text-gray-600 rounded-lg shadow-md p-6'>
-        {image && (
-          <Image
-            src={image}
-            alt={name}
-            width={250}
-            height={250}
-            className='rounded-lg'
-            priority
-          />
-        )}
-        <h3 className=' text-xl text font-medium'>{name}</h3>
-        <button
-          className='p-3 bg-neutral-400 rounded-md font-bold text-black'
-          onClick={() => {
-            location.reload();
-          }}
-        >
-          PURE EVIL
-        </button>
-      </div>
-    </div>
+    <>
+      {image && (
+        <div className='mx-auto'>
+          <div className='flex flex-col bg-neutral-300 gap-3 text-center text-gray-600 rounded-lg shadow-md p-6'>
+            {/* {image && ( */}
+            <Image
+              src={image}
+              alt={name}
+              width={250}
+              height={250}
+              className='rounded-lg'
+              priority
+            />
+            {/* // )} */}
+            <h3 className=' text-xl text font-medium'>{name}</h3>
+            <button
+              className='p-3 bg-neutral-400 rounded-md font-bold text-black'
+              onClick={() => {
+                location.reload();
+              }}
+            >
+              PURE EVIL
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }

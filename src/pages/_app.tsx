34d7d374649c3a5d7d43d8e9431c3d62ab402203
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2022-10-11 17:39:52 -0700

    add next-plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css';
 import type { AppType } from 'next/dist/shared/lib/utils';
 import { trpc } from '@/utils/trpc';
+import PlausibleProvider from 'next-plausible';
 import Header from '@/components/Layout/Header';
 import Footer from '@/components/Layout/Footer';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <>
+    <PlausibleProvider domain='evilist-of-ricks.gudt.io'>
       <div className='flex flex-col h-screen'>
         <Header />
         <div className='grow'>
@@ -14,7 +15,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         </div>
         <Footer />
       </div>
-    </>
+    </PlausibleProvider>
   );
 };
 

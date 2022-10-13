e523ff5b4e78851d4160ba87fb8cc7cd7e58bb0e
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2022-10-13 16:32:02 -0700

    util boiler fix

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,8 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '../pages/api/trpc/[trpc]';
+import { loggerLink } from '@trpc/client/links/loggerLink';
+import type { inferProcedureOutput } from '@trpc/server';
 
 function getBaseUrl() {
   if (typeof window !== 'undefined')
@@ -21,25 +23,50 @@ function getBaseUrl() {
 
 export const trpc = createTRPCNext<AppRouter>({
   config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+
     return {
+      /**
+       * @link https://trpc.io/docs/links
+       */
       links: [
-        httpBatchLink({
-          /**
-           * If you want to use SSR, you need to use the server's full URL
-           * @link https://trpc.io/docs/ssr
-           **/
-          url: `${getBaseUrl()}/api/trpc`,
+        // adds pretty logs to your console in development and logs errors in production
+        loggerLink({
+          enabled: (opts) =>
+            (process.env.NODE_ENV === 'development' &&
+              typeof window !== 'undefined') ||
+            (opts.direction === 'down' && opts.result instanceof Error),
         }),
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            if (ctx?.req) {
+              // on ssr, forward client's headers to the server
+              return {
+                ...ctx.req.headers,
+                'x-ssr': '1',
+              };
+            }
+            return {};
+          },
+        }),
+        // getEndingLink(ctx),
       ],
       /**
-       * @link https://react-query-v3.tanstack.com/reference/QueryClient
-       **/
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+       * @link https://trpc.io/docs/data-transformers
+       */
+      // transformer: superjson,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
     };
   },
   /**
    * @link https://trpc.io/docs/ssr
-   **/
+   */
   ssr: true,
   responseMeta({ ctx, clientErrors }) {
     if (clientErrors.length) {
@@ -48,6 +75,7 @@ export const trpc = createTRPCNext<AppRouter>({
         status: clientErrors[0].data?.httpStatus ?? 500,
       };
     }
+
     // cache request for 1 day + revalidate once every second
     const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
     return {
@@ -57,3 +85,11 @@ export const trpc = createTRPCNext<AppRouter>({
     };
   },
 });
+
+/**
+ * This is a helper method to infer the output of a query resolver
+ * @example type HelloOutput = inferQueryOutput<'hello'>
+ */
+export type inferQueryOutput<
+  TRouteKey extends keyof AppRouter['_def']['queries'],
+> = inferProcedureOutput<AppRouter['_def']['queries'][TRouteKey]>;
